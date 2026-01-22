c187851a56d31f8e4323cebbd4f6e2f110c6500e
chefberke <berkekanber@gmail.com>
2026-01-22 20:48:25 +0300

    feat: integrate Vercel Analytics for enhanced tracking
    - Added @vercel/analytics dependency to package.json and package-lock.json for improved analytics capabilities.
    - Implemented Analytics component in the layout for tracking user interactions.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "@/styles/globals.css";
 import ClientThemeProvider from "@/components/ClientThemeProvider";
+import { Analytics } from "@vercel/analytics/next"
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -55,6 +56,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
         suppressHydrationWarning
       >
+        <Analytics />
         <ClientThemeProvider>{children}</ClientThemeProvider>
       </body>
     </html>
